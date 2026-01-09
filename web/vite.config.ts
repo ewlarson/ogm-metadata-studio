93f2fe487ceabf25390a6f99b0f78ee400083bc1
Eric Larson <ewlarson@gmail.com>
2026-01-09 15:19:47 -0600

    Bug Fix: test coverage

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -70,31 +70,19 @@ export default defineConfig({
       provider: 'v8',
       all: true,
       reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.{ts,tsx}'],
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
       exclude: [
         'src/vite-env.d.ts',
         'src/main.tsx',
-        'src/App.tsx',
-        'coverage/**',
-        'dist/**',
-        '**/[.]**',
-        'packages/*/test?(s)/**',
         '**/*.d.ts',
-        '**/virtual:*',
-        '**/__x00__*',
-        '**/\x00*',
-        'cypress/**',
-        'test?(s)/**',
-        'test?(-*).?(c|m)[jt]s?(x)',
-        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
-        '**/__tests__/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-        '**/vitest.{workspace,projects}.[jt]s?(on)',
-        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}'
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        'src/test/**'
       ],
     },
   },
 } as any);
+
 
 
 
